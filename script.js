0141ef51cf14d39b0117fbe066de6af9a8c5d0d5
Amina-design-cloud <panda04190927@gmail.com>
2025-05-21 23:08:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 function renderQuestion(containerId, questions, startId) {
   let currentId = startId;
   const box = document.getElementById(containerId);
+
   function showQuestion(id) {
     const q = questions[id];
     box.innerHTML = `<p>${q.text}</p>`;
     q.options.forEach(opt => {
       const btn = document.createElement("button");
       btn.textContent = opt.text;
+      btn.className = "card";
       btn.onclick = () => {
         if (opt.next) showQuestion(opt.next);
-        else box.innerHTML = `<p><strong>建議結果：</strong>${opt.result}</p>`;
+        else box.innerHTML = `<p><strong>建議結果：</strong>${opt.result}</p><a href="index.html" class="card">回首頁</a>`;
       };
       box.appendChild(btn);
     });
   }
+
   showQuestion(currentId);
 }
+
